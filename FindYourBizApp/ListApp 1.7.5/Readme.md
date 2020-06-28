52c754ea0f9d25e9132f8d052cbaf51d01557768
Heavens Milien <heavens.milien@gmail.com>
2020-06-28 15:35:09 -0400

    Initial commit

---
+++ FindYourBizApp/ListApp 1.7.5/Readme.md
@@ -0,0 +1,5 @@
+Document links: https://docs/inspireui.com/listapp
+
+Support Ticket: https://github.com/inspireui/support/issues
+
+Download CeBuilder (free tool to build app): https://inspireui.com 
---
+++ FindYourBizApp/ListApp 1.7.5/source/babel.config.js
@@ -0,0 +1,18 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'babel-plugin-module-resolver', {
+          alias: {
+            'images': './assets/Images',
+          }
+        }
+      ],
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      '@babel/plugin-transform-runtime',
+    ]
+  };
+};
